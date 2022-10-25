8c29dbda2c724f6dd0b30a39cfbab079ba159621
Omar Cardoze <cardozeomar@gmail.com>
2022-10-25 11:20:37 -0500

    delete src folder

---
+++ data/projects.ts
@@ -0,0 +1,52 @@
+import { Project } from '../types'
+
+export const PROJECTS: Project[] = [
+  {
+    title: 'Ejemplo',
+    url: 'https://github.com',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius consequuntur esse accusamus odit soluta earum illum velit minima ipsum debitis?.',
+    languaje: 'Javascript',
+    tags: 'Example-tag',
+    category: 'frontend'
+  },
+  {
+    title: 'Ejemplo',
+    url: 'https://github.com',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius consequuntur esse accusamus odit soluta earum illum velit minima ipsum debitis?.',
+    languaje: 'Typescript',
+    tags: 'Example-tag',
+    category: 'frontend'
+  },
+  {
+    title: 'Ejemplo',
+    url: 'https://github.com',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius consequuntur esse accusamus odit soluta earum illum velit minima ipsum debitis?.',
+    languaje: 'Javascript',
+    tags: 'Example-tag',
+    category: 'frontend'
+  },
+  {
+    title: 'Ejemplo',
+    url: 'https://github.com',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius consequuntur esse accusamus odit soluta earum illum velit minima ipsum debitis?.',
+    languaje: 'Typescript',
+    tags: 'Example-tag',
+    category: 'frontend'
+  },
+  {
+    title: 'Ejemplo',
+    url: 'https://github.com',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius consequuntur esse accusamus odit soluta earum illum velit minima ipsum debitis?.',
+    languaje: 'Python',
+    tags: 'Example-tag',
+    category: 'backend'
+  },
+  {
+    title: 'Ejemplo',
+    url: 'https://github.com',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius consequuntur esse accusamus odit soluta earum illum velit minima ipsum debitis?.',
+    languaje: 'Javascript',
+    tags: 'Example-tag',
+    category: 'frontend'
+  }
+]
---
+++ types.ts
@@ -0,0 +1,8 @@
+export interface Project {
+  title: string
+  url: string
+  description: string
+  languaje: string
+  tags: string
+  category: string
+}
