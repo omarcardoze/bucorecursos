7be3e6c32772c6bd70409859da8f85dd0238f357
Omar Cardoze <cardozeomar@gmail.com>
2022-10-29 18:57:52 -0500

    Fix navbar padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,8 @@ export default function Navbar() {
   return (
     <nav className='w-full border-b dark:border-gray-700'>
       <div className='container mx-auto w-full'>
-        <ul className='w-full flex text-3xl md:text-base items-center py-7 md:flex flex-row justify-center gap-3'>
-          <li className='text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-400  cursor-pointer text-base lg:text-lg pt-10 md:pt-0'>
+        <ul className='w-full flex text-3xl md:text-base items-center md:flex flex-row justify-center gap-3 py-5'>
+          <li className='text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-400  cursor-pointer text-base lg:text-lg md:pt-0'>
             <Link
               href='https://github.com/OmarCardoze/Dir-repo'
               target='_blank'
@@ -14,10 +14,10 @@ export default function Navbar() {
               Github
             </Link>
           </li>
-          <li className='text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-400 cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10'>
+          <li className='text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-400 cursor-pointer text-base lg:text-lg  md:pt-0 md:ml-5 lg:ml-10'>
             <Link href='/projects'>Proyectos</Link>
           </li>
-          <li className='text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-400  cursor-pointer text-base lg:text-lg pt-10 md:pt-0 md:ml-5 lg:ml-10'>
+          <li className='text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-400  cursor-pointer text-base lg:text-lg md:pt-0 md:ml-5 lg:ml-10'>
             <Link href='/resources'>Recursos</Link>
           </li>
         </ul>
