1acffffc416a562bc54f86c8d182f06b77df2ed9
Omar Cardoze <cardozeomar@gmail.com>
2022-10-29 18:43:01 -0500

    Fix mobile view

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className='w-full border-b dark:border-gray-700'>
       <div className='container mx-auto w-full'>
-        <ul className='w-full flex text-3xl md:text-base items-center py-7 md:flex flex-col md:flex-row justify-center fixed md:relative top-0 bottom-0 left-0 right-0 bg-white md:bg-transparent z-20'>
+        <ul className='w-full flex text-3xl md:text-base items-center py-7 md:flex flex-row justify-center gap-3'>
           <li className='text-black hover:text-gray-600 dark:text-white dark:hover:text-gray-400  cursor-pointer text-base lg:text-lg pt-10 md:pt-0'>
             <Link
               href='https://github.com/OmarCardoze/Dir-repo'
