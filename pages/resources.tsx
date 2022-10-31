c2c8d8915264da78338d9f15c6903acb08976c71
Omar Cardoze <cardozeomar@gmail.com>
2022-10-31 18:58:45 -0500

    feat: new page resourses

---
+++ pages/resources.tsx
@@ -0,0 +1,10 @@
+
+const Resources = () => {
+  return (
+    <div className="h-screen grid place-content-center">
+        <h1 className="text-4xl">En Construcción</h1>
+    </div>
+  )
+}
+
+export default Resources
