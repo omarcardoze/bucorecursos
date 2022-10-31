cb2da747bb15bce9c8b92f4a045028548223538e
Omar Cardoze <cardozeomar@gmail.com>
2022-10-30 20:59:46 -0500

    add open graph image

--- components/HeadPage.tsx
+++ components/HeadPage.tsx
@@ -8,6 +8,10 @@ const HeadPage = () => {
         name='description'
         content='Proyecto que busca potenciar la contribuciones a proyectos open source'
       />
+      <meta property="og:title" content="Comunidad de desarrollo en español" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/opengraph-ops.jpg" />
+      <meta property="og:url" content="https://dirrepo.vercel.app" />
       <link rel='icon' href='/favicon.ico' />
     </Head>
   )
