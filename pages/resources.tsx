1f0c04585a3a8e28af48b7666e4de4c74e33aedb
Omar Cardoze <cardozeomar@gmail.com>
2022-11-07 20:11:55 -0500

    Delete example text in resourse component

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -2,11 +2,6 @@ const Resources = () => {
   return (
     <div className='h-screen grid place-content-center'>
       <h1 className='text-4xl'>En Construcción</h1>
-      <p>Pronto!</p>
-      <p>
-        Lorem ip adipisci fuga ducimus numquam quae, non minima consequuntur atque
-        quo?
-      </p>
     </div>
   )
 }
