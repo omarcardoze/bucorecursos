c3ce238557836cdbad08e2687562259da0bcd11d
Omar Cardoze <cardozeomar@gmail.com>
2022-11-07 19:53:37 -0500

    Add lint config4

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,7 +1,9 @@
 const Resources = () => {
   return (
     <div className='h-screen grid place-content-center'>
-        <h1 className='text-4xl'>En Construcción</h1>
+      <h1 className='text-4xl'>En Construcción</h1>
+      <p>Pronto!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, commodi? Eos aut qui optio magni, quis voluptatum illum! Temporibus provident adipisci fuga ducimus numquam quae, non minima consequuntur atque quo?</p>
     </div>
   )
 }
