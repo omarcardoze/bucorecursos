fa9adf36fff07fbcba6e7cd2f7d9a0961f52147d
Omar Cardoze <cardozeomar@gmail.com>
2022-11-07 19:46:44 -0500

    Add lint config2

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,7 +1,7 @@
 const Resources = () => {
   return (
     <div className='h-screen grid place-content-center'>
-      <h1 className='text-4xl'>En Construcción</h1>
+        <h1 className='text-4xl'>En Construcción</h1>
     </div>
   )
 }
