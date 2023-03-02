27d1edc7f79eee8d01a9c749249b60b70af2e897
Omar Cardoze <cardozeomar@gmail.com>
2023-03-02 16:56:57 -0500

    Change img to Next Image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image'
+
 export const Footer = () => {
   return (
     <footer className='w-full h-24 flex justify-center items-center'>
       <div className='contaiener'>
         <a href='https://github.com/OmarCardoze/Dir-repo'>
-          <img
+          <Image
             alt='GitHub Repo stars'
             src='https://img.shields.io/github/stars/OmarCardoze/Dir-repo?style=social'
+            width={82}
+            height={20}
           />
         </a>
       </div>
