8a09ace6c5b46e809b320d9898d00d02cebac759
Omar Cardoze <cardozeomar@gmail.com>
2023-03-02 16:25:45 -0500

    Change background color

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,9 +7,9 @@ interface Props {
 export const Header = ({ title }: Props) => {
   return (
     <>
-      <header className='container w-full h-[20rem] flex items-center justify-center flex-col relative'>
+      <header className='w-full h-[20rem] flex items-center justify-center flex-col relative bg-yellow-400'>
         <Navbar />
-        <h1 className='text-center py-5 leading-tight tracking-tight text-4xl'>
+        <h1 className='text-center py-5 leading-tight tracking-tight text-4xl font-bold uppercase'>
           {title}
         </h1>
       </header>
