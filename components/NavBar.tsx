11daa047b07c9f1f1c1a7157828ce04e561d663a
Omar Cardoze <cardozeomar@gmail.com>
2023-03-02 16:57:49 -0500

    Change img to Next Image

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image'
+
 export const Navbar = () => {
   return (
     <nav className='container w-full flex justify-center px-2 sm:px-4 py-2.5 bg-transparent absolute top-0'>
       <div className='container flex flex-wrap items-center justify-between mx-auto'>
         <a href='https://bucorecursos.vercel.app/' className='flex items-center'>
-          <img src='/logo.svg' className='h-6 mr-3 sm:h-14' alt='Logo' />
+          <Image
+            src='/logo.svg'
+            className='mr-3 sm:h-14'
+            width={35}
+            height={35}
+            alt='Logo'
+          />
           <span className='self-center text-xl font-semibold whitespace-nowrap text-black'>
             Buco Recursos
           </span>
