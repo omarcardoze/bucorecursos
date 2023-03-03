6c1fe6099ff5bb349aa9b0d5f59bccf2a342249c
Omar Cardoze <cardozeomar@gmail.com>
2023-03-03 17:39:48 -0500

    Change limit

--- hooks/useLimit.ts
+++ hooks/useLimit.ts
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
 
 export default function useLimit(targetRef: any) {
-  const [limit, setLimit] = useState<number>(5)
+  const [limit, setLimit] = useState<number>(20)
   useEffect(() => {
     const observer = new IntersectionObserver(([target]) => {
       if (target.isIntersecting) {
-        setLimit((limit) => limit + 5)
+        setLimit((limit) => limit + 20)
       }
     })
     observer.observe(targetRef.current)
 
-    return () => observer.disconnect()
+    return () => {
+      observer.disconnect()
+    }
   }, [targetRef])
 
   return limit
